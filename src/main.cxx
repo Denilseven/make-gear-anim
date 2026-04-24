ae8d3aef7a1f2ea3ed2f2ea858c6998e6e130b89
Denilseven <104108067+Denilseven@users.noreply.github.com>
2026-04-24 10:01:01 -0400

    add `Part` struct and figure out `DrawTexturePro`
    it took me an embarrassing amount of time to
    figure out where to set the `pivot` to...

--- src/main.cxx
+++ src/main.cxx
@@ -1,16 +1,67 @@
+#include <iostream>
 #include <raylib.h>
+#include <string>
+#include <vector>
 
 #define WIN_WIDTH 1000
 #define WIN_HEIGHT 1000
+#define TRANSPARENT (Color){1, 1, 1, 0}
 
-int main(){
+struct Part {
+    std::string name{};
+    Rectangle bounds{};
+    Vector2 pivot{};
+    std::vector<Vector2> attachmentPoints{};
+
+    std::string partAttachedTo{};
+    int attachedToPointN{0};
+
+    Vector2 position{};
+    Vector2 scale{1.0f, 1.0f};
+    float rotation{0.0f};
+};
+
+
+int main() {
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "Awesome");
     SetTargetFPS(60);
 
-    while(!WindowShouldClose()){
-        BeginDrawing();
+    Texture2D texture = LoadTexture("assets/image.png");
+    std::vector<Part> parts{};
+     
+    parts.push_back(
+        (Part){
+            .name = "Heheh",
+            .bounds = (Rectangle){0, 0, 200, 200},
+            .pivot = (Vector2){100, 200},
+            .position = (Vector2){500, 500},
+        }
+    );
+    parts[0].attachmentPoints.push_back((Vector2){100, 200});
+    parts[0].attachmentPoints.push_back((Vector2){0, 200});
 
+    while(!WindowShouldClose()) {
+        BeginDrawing();
         ClearBackground(LIGHTGRAY);
+
+        parts[0].scale = (Vector2){(float)GetMouseX() / 100.0f, (float)GetMouseY() / 100.0f};
+        // parts[0].scale = (Vector2){1.0f, 1.0f};
+
+        for (int i = 0; i < parts.size(); i++) {
+            Part part = parts[i];
+            DrawTexturePro(
+                texture,
+                part.bounds,
+                (Rectangle){part.position.x, part.position.y, part.bounds.width * part.scale.x, part.bounds.height * part.scale.y},
+                (Vector2){part.pivot.x * part.scale.x, part.pivot.y * part.scale.y},
+                GetTime() * 100,
+                ORANGE
+            );
+            DrawCircleGradient(part.position, 10, TRANSPARENT, BLUE);
+            for (int j = 0; j < part.attachmentPoints.size(); j++) {
+                DrawCircleGradient(part.attachmentPoints[j], 7, TRANSPARENT, RED);
+            }
+        }
 
         EndMode2D();
 
@@ -21,6 +72,7 @@ int main(){
         EndDrawing();
     }
 
+    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
