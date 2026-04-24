d8d61b616304a9f23ffd38ce31786e75f414a716
Denilseven <104108067+Denilseven@users.noreply.github.com>
2026-04-24 16:14:47 -0400

    add some high school grade maths
    to rotate the extra attachment points with each part
    
    thanks to `Mario's Math Tutoring` and `Freya Holmér` on yt :P

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,8 @@
+// TODO: take `scale` into account for `attachmentPoints` positioning
+
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 #include <string>
 #include <vector>
 
@@ -21,6 +24,14 @@ struct Part {
     float rotation{0.0f};
 };
 
+Vector2 rotateAround(const Vector2& point, const Vector2& center, float degrees) {
+    float radians = degrees * (M_PI / 180);
+    Vector2 result = (Vector2){
+        result.x = (point.x-center.x)*std::cos(radians) - (point.y-center.y)*std::sin(radians),
+        result.y = (point.x-center.x)*std::sin(radians) + (point.y-center.y)*std::cos(radians)
+    };
+    return result + center;
+}
 
 int main() {
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "Awesome");
@@ -33,19 +44,22 @@ int main() {
         (Part){
             .name = "Heheh",
             .bounds = (Rectangle){0, 0, 200, 200},
-            .pivot = (Vector2){100, 200},
+            .pivot = (Vector2){100, 100},
             .position = (Vector2){500, 500},
         }
     );
-    parts[0].attachmentPoints.push_back((Vector2){100, 200});
-    parts[0].attachmentPoints.push_back((Vector2){0, 200});
+    parts[0].attachmentPoints.push_back((Vector2){0, -100});
+    parts[0].attachmentPoints.push_back((Vector2){100, 0});
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(LIGHTGRAY);
 
-        parts[0].scale = (Vector2){(float)GetMouseX() / 100.0f, (float)GetMouseY() / 100.0f};
-        // parts[0].scale = (Vector2){1.0f, 1.0f};
+        // parts[0].scale = (Vector2){(float)GetMouseX() / 100.0f, (float)GetMouseY() / 100.0f};
+        parts[0].scale = (Vector2){1.0f, 1.0f};
+        parts[0].rotation = GetTime() * 25;
+        // parts[0].rotation = 40;
+        parts[0].position = GetMousePosition();
 
         for (int i = 0; i < parts.size(); i++) {
             Part part = parts[i];
@@ -54,12 +68,19 @@ int main() {
                 part.bounds,
                 (Rectangle){part.position.x, part.position.y, part.bounds.width * part.scale.x, part.bounds.height * part.scale.y},
                 (Vector2){part.pivot.x * part.scale.x, part.pivot.y * part.scale.y},
-                GetTime() * 100,
+                part.rotation,
                 ORANGE
             );
             DrawCircleGradient(part.position, 10, TRANSPARENT, BLUE);
+
             for (int j = 0; j < part.attachmentPoints.size(); j++) {
-                DrawCircleGradient(part.attachmentPoints[j], 7, TRANSPARENT, RED);
+                DrawCircleGradient(
+                    rotateAround(
+                        part.attachmentPoints[j] + part.position,
+                        part.position,
+                        part.rotation
+                    ),
+                    7 * (j + 1), TRANSPARENT, RED);
             }
         }
 
